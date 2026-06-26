85425e6b15821b58d49c66f8cd2462be7181c302
nguyenhiennnnn <hienkoi05@gmail.com>
2026-06-26 13:56:17 +0700

    feat: create login page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthLayout } from "./layouts/auth-layout";
 import { LoginPage } from "./features/auth/components/login-page";
 import { RegisterPage } from "./features/auth/components/register-page";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query-client";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
